61f4f7557ac251dd9e81f4f187014c09a15af3b0
Jessica Nguyen <jessica.stevens2115@gmail.com>
2022-02-21 15:20:41 -0700

    Flashcard work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
+import './App.css'
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <FlashcardList flashcards={flashcards} />
     </div>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is the fastest animal on land?',
+    answer: 'Cheetah',
+    options: [
+      'Gazelle',
+      'Rhino',
+      'Cheetah',
+      'Hippo'
+    ]
+  },
+  {
+    id: 3,
+    question: 'How fast can a peregrine falcon dive bomb?',
+    answer: '150 mph',
+    options: [
+      '80 mph',
+      '120 mph',
+      '95 mph',
+      '150 mph'
+    ]
+  },
+]
 
 export default App;
---
+++ src/Flashcard.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+export default function Flashcard({ flashcard }) {
+    const [flip, setFlip] = useState(false)
+    const [height, setHeight] = useState('inital')
+
+    const frontEl = useRef()
+    const backEl = useRef()
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height
+        const backHeight = backEl.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+
+    useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+
+    return (
+        <div 
+            className={`card ${flip ? 'flip' : ''}`}
+            onClick={() => setFlip(!flip)}>
+
+            <div className="front" ref={frontEl}>
+                {flashcard.question}
+                <div className="flashcard-options">
+                    {flashcard.options.map(option => {
+                        return <div className="flashcard-option">{option}</div>
+                    })}
+                </div>
+            </div>
+            <div className="back" ref={backEl}>{flashcard.answer}</div>
+        </div>
+  )
+}
